93dc74461ba05a13f5134e3dc31d3889bcccbae7
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-05-13 22:23:05 -0700

    Add xtask to generate and open an HTML code coverage report
    We'll add `lcov` format reports and uploading to a coverage provider later.
    
    See:
    - https://doc.rust-lang.org/rustc/instrument-coverage.html
    - https://github.com/mozilla/grcov#generate-a-coverage-report-from-coverage-artifacts
    - https://blog.rng0.io/how-to-do-code-coverage-in-rust

--- xtask/src/install.rs
+++ xtask/src/install.rs
@@ -5,10 +5,9 @@ use crate::utils::{project_root, verbose_cd};
 
 pub fn rust_dependencies() -> Result<()> {
     let sh = Shell::new()?;
-    let root = project_root();
-    verbose_cd(&sh, &root);
+    verbose_cd(&sh, project_root());
 
-    cmd!(sh, "rustup component add clippy rustfmt").run()?;
-    cmd!(sh, "cargo install cargo-insta cargo-nextest").run()?;
+    cmd!(sh, "rustup component add clippy llvm-tools-preview rustfmt").run()?;
+    cmd!(sh, "cargo install cargo-insta cargo-nextest grcov").run()?;
     Ok(())
 }
