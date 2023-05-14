3baef504edcc7006ad843dbefc597e227a574a74
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-05-14 10:35:24 -0700

    Ignore the xtask code when generating coverage report
    In order to have `grcov` properly ignore certain packages, the LLVM
    profile files must exist within the project directory structure, so
    we can't use our temp_dir trick to keep things clean. Instead, I've
    made the `find_markdown_files` function generic so you can just supply
    the desired extension as an argument, and the returned paths will
    automatically be made relative to the given directory.

--- xtask/src/fixup.rs
+++ xtask/src/fixup.rs
@@ -1,10 +1,9 @@
-use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use xshell::{cmd, Shell};
 
-use crate::utils::{project_root, verbose_cd};
+use crate::utils::{find_files, project_root, verbose_cd};
 
 pub fn everything() -> Result<()> {
     spelling()?; // affects all file types; run this first
@@ -28,35 +27,14 @@ pub fn markdown() -> Result<()> {
 
 fn format_markdown() -> Result<()> {
     let sh = Shell::new()?;
-    let root = project_root();
-    verbose_cd(&sh, &root);
+    verbose_cd(&sh, project_root());
 
-    let markdown_files = find_markdown_files(sh.current_dir())?;
-    let relative_paths: Vec<PathBuf> = markdown_files
-        .into_iter()
-        .filter_map(|path| path.strip_prefix(&root).ok().map(PathBuf::from))
-        .collect();
-
+    let markdown_files = find_files(sh.current_dir(), "md")?;
     cmd!(sh, "prettier --prose-wrap always --write")
-        .args(&relative_paths)
+        .args(markdown_files)
         .run()?;
 
     Ok(())
-}
-
-fn find_markdown_files<P: AsRef<Path>>(dir: P) -> Result<Vec<PathBuf>> {
-    let mut result = Vec::new();
-    for entry in fs::read_dir(dir)? {
-        let path = entry?.path();
-
-        if path.is_dir() {
-            let mut subdir_files = find_markdown_files(&path)?;
-            result.append(&mut subdir_files);
-        } else if path.is_file() && path.extension().map_or(false, |ext| ext == "md") {
-            result.push(path);
-        }
-    }
-    Ok(result)
 }
 
 pub fn rust() -> Result<()> {
