f75430498d56bb4e898803f30f40facd76d362a9
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-05-22 10:12:35 -0700

    Add criterion integration for benchmarks
    There is a bit of ugliness to allow passing arguments through to
    criterion without issues, in that we must explicitly put `bench = false`
    in all of the Cargo.toml files for the crates in the workspace. It's
    something that is being looked at upstream.
    
    See:
    - https://github.com/bheisler/criterion.rs
    - https://bheisler.github.io/criterion.rs/book/faq.html#cargo-bench-gives-unrecognized-option-errors-for-valid-command-line-options
    - https://github.com/rust-lang/rust/issues/47241

--- crates/mylib/src/lib.rs
+++ crates/mylib/src/lib.rs
@@ -13,6 +13,36 @@ pub fn shuffle_array(nums: &mut [i32]) {
     nums.shuffle(&mut rng);
 }
 
+// Example fib functions for benchmarking.
+
+// #[inline]
+// #[must_use]
+// pub fn fibonacci(n: u64) -> u64 {
+//     match n {
+//         0 => 0,
+//         1 => 1,
+//         n => fibonacci(n - 1) + fibonacci(n - 2),
+//     }
+// }
+
+#[inline]
+#[must_use]
+pub fn fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+
+    let mut a = 0;
+    let mut b = 1;
+
+    for _ in 1..n {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -20,5 +50,10 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(add(10, 32), 42);
+    }
+
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(fibonacci(20), 6765);
     }
 }
